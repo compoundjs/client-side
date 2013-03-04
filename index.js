31adb3943f35acb540fd22a9da9411504fce03a4
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-05 01:01:40 +0700

    Adapt code to independent library

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var generator = require('./lib/generator');
+var fs = require('fs');
 
 exports.init = function(compound) {
-    compound.generators.register('clientside', generator);
+    with (compound.generators) {
+        register('clientside', generator);
+        compound.on('initializers', function () {
+            if (fs.existsSync(compound.root + '/client-side.js')) {
+                fs.unlinkSync(compound.root + '/client-side.js');
+            }
+            init(compound);
+            perform('cs', []);
+        });
+    }
+    with (compound.helpers) {
+        contentFor('javascripts', javascriptIncludeTag('compound'));
+    }
 };
